db351bf8f470886e9b82597c36f088f2c7ff9190
eubank87 <eubank.ashley@gmail.com>
2021-02-15 15:23:07 -0800

    found new favicon, dialing in on style sheets

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Table from './components/Table';
 import Header from "./components/Header"
 
 function App() {
-  return (<div>
+  return (<div className="main">
             <div>
               <Header/>
             </div>
